9c7a9d766de93752dc38ee09ed0359550d06eed1
ehlwl <60280351+ehlwl@users.noreply.github.com>
2021-01-23 16:25:05 -0800

    Update skillPractive.java

--- src/_04_practice/_1_skill_practice/skillPractive.java
+++ src/_04_practice/_1_skill_practice/skillPractive.java
@@ -8,9 +8,9 @@ public class skillPractive {
 		public static void main(String[] args) {
 
 		skillPractive skills = new skillPractive();
-		           skills.skill1();
-		           skills.skill2();
-		           skills.skill3();
+		           //skills.skill1();
+		         //  skills.skill2();
+		         //  skills.skill3();
 		           skills.skill4();
 		           skills.skill5();
 		}
@@ -61,24 +61,36 @@ public class skillPractive {
 
 		void skill3() { // Get a random number that is less than 20 and print it to the console
 
-		
+		Random r = new Random(); 
+		int x = r.nextInt(20);
+		System.out.println(x);
 
 		// Get another random number that is less than 10 and print it to the console
-
-		
+		int y = r.nextInt(10);
+		System.out.println(y);
 
 		// Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction
-
+		if(y<x) {
+			JOptionPane.showMessageDialog(null, x-y);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, y-x);
+		}
 
 
 		}
 
 		void skill4() { // In a pop-up, ask the user for the city they live in
 
-
-
+			String c = JOptionPane.showInputDialog("what city are you living in?");
+			
 		// If they answered "San Diego", tell them they live in America's Finest City
-
+			if (c.equals("San Diego")) {
+				JOptionPane.showMessageDialog(null, "wow your living in AMerica's finest city");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "move to San Diego");
+			}
 
 
 		// Otherwise, tell them to move to San Diego
@@ -87,8 +99,13 @@ public class skillPractive {
 
 		// Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation."
 
-
-
+			int car = 1;
+			if (car==0) {
+				JOptionPane.showMessageDialog(null, "I bet you use public transportation");
+			}
+			else if(car==1) {
+				JOptionPane.showMessageDialog(null, "toyota/oddysay");
+			}
 		// If there is 1 car, use a pop-up to display the make/model of the car
 
 
